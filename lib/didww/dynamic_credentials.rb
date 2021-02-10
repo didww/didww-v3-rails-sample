cfbb8c9e80c437445fd8be04206ba37ce57d1783
Denis Talakevich <senid231@gmail.com>
2021-02-10 16:04:13 +0200

    allow provide custom base url

--- lib/didww/dynamic_credentials.rb
+++ lib/didww/dynamic_credentials.rb
@@ -10,6 +10,10 @@ module DIDWW
       def api_mode
         RequestStore.store['api_mode']&.to_sym || DEFAULT_MODE
       end
+
+      def api_base_url
+        ENV['DIDWW_API_URL'] || BASE_URLS[api_mode]
+      end
     end
   end
 end
