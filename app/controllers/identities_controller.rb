dbcf329708c2f39c84f8da0fcdf2d5198337fd74
paratskiy <bog4242@gmail.com>
2021-02-22 11:08:08 +0200

    create identity new/edit/destroy

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,4 +1,33 @@
 class IdentitiesController < DashboardController
+  before_action :assign_params, only: [:create, :update]
+
+  def create
+    if resource.save
+      flash[:success] = 'Identity was successfully created.'
+      redirect_to identity_path(resource)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if resource.save
+      flash[:success] = 'Identity was successfully updated.'
+      redirect_to identity_path(resource)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if resource.destroy
+      flash[:success] = 'Identity was successfully deleted.'
+      redirect_to identities_path
+    else
+      flash[:danger] = 'Failed to delete Identity: ' + resource.errors[:base].join('. ')
+      redirect_back fallback_location: identity_path(resource)
+    end
+  end
 
   private
 
@@ -16,6 +45,27 @@ class IdentitiesController < DashboardController
       #   :'did_group_type.id'
       # ]
     })
+  end
+
+  def assign_params
+    resource.attributes = resource_params
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def resource_params
+    params.require(:identity).permit(
+      :first_name,
+      :last_name,
+      :phone_number,
+      :id_number,
+      :birth_date,
+      :company_name,
+      :company_reg_number,
+      :vat_id,
+      :description,
+      :personal_tax_id,
+      :identity_type
+    )
   end
 
   def apply_sorting(collection)
