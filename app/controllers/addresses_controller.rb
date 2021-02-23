fba264a145634a3f4b8d2fc6cffd4c9833a1270d
paratskiy <bog4242@gmail.com>
2021-02-23 10:15:25 +0200

    create address new/edit/destroy

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,33 @@
 class AddressesController < DashboardController
+  before_action :assign_params, only: [:create, :update]
+
+  def create
+    if resource.save
+      flash[:success] = 'Address was successfully created.'
+      redirect_to address_path(resource)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if resource.save
+      flash[:success] = 'Address was successfully updated.'
+      redirect_to address_path(resource)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if resource.destroy
+      flash[:success] = 'Address was successfully deleted.'
+      redirect_to identities_path
+    else
+      flash[:danger] = 'Failed to delete Address: ' + resource.errors[:base].join('. ')
+      redirect_back fallback_location: address_path(resource)
+    end
+  end
 
   private
 
@@ -15,6 +44,26 @@ class AddressesController < DashboardController
       #   :'did_group_type.id'
       # ]
     })
+  end
+
+  def assign_params
+    country = DIDWW::Resource::Country.load(id: resource_params[:country_id])
+    identity = DIDWW::Resource::Identity.load(id: resource_params[:identity_id])
+    resource.attributes = resource_params.except(:country_id, :identity_id)
+    resource.relationships.country = country
+    resource.relationships.identity = identity
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def resource_params
+    params.require(:address).permit(
+      :city_name,
+      :postal_code,
+      :address,
+      :description,
+      :identity_id,
+      :country_id
+    )
   end
 
   def apply_sorting(collection)
