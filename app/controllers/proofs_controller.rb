eda628a07aedb4e7c1ae877c423bde007965ffb7
paratskiy <bog4242@gmail.com>
2021-03-05 13:43:32 +0200

    fix bug with adding proof without params

--- app/controllers/proofs_controller.rb
+++ app/controllers/proofs_controller.rb
@@ -1,12 +1,12 @@
 class ProofsController < DashboardController
-  before_action :assign_params, only: [:create]
 
   def create
-    if resource.save
+    form = ProofForm.new(resource_params)
+    if form.save
       flash[:success] = 'Proof was successfully created.'
       render status: 201, js: "window.location = '#{identity_path(resource_params[:identity_id])}'"
     else
-      render status: 422, json: { errors: resource.errors.messages }
+      render status: 422, json: { errors: form.errors.messages }
     end
   end
 
@@ -31,16 +31,6 @@ class ProofsController < DashboardController
         :entity
       ]
     })
-  end
-
-  def assign_params
-    proof_type = DIDWW::Resource::ProofType.load(id: resource_params[:proof_type_id])
-    entity = DIDWW::Resource::Identity.load(id: resource_params[:identity_id])
-    file_ids = DIDWW::Resource::EncryptedFile.upload(resource_params[:files], resource_params[:encryption_fingerprint])
-    files = file_ids.map { |id| DIDWW::Resource::EncryptedFile.load(id: id) }
-    resource.relationships.proof_type = proof_type
-    resource.relationships.entity = entity
-    resource.relationships.files = files
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
