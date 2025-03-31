239598e77fbb7de02ab771e035c9dc603637ac91
Igor Gonchar <igor.gonchar@gmail.com>
2025-03-31 12:51:16 +0200

    fix "Add proofs" modals

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -2,5 +2,15 @@
 class BalancesController < DashboardController
   def show
     reload_user_balance
+
+    respond_to do |format|
+      format.html do
+        render 'show'
+      end
+
+      format.json do
+        render json: session[:cached_balance]
+      end
+    end
   end
 end
--- app/views/balances/show.json.jbuilder
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-json.merge! session[:cached_balance].map { |k, v| [k, number_to_currency(v)] }.to_h
-json.url balance_url(format: :json)
